936b057b82a155d5163892bb47781dbf1ca7adb0
TopMak <26412430+TopMak@users.noreply.github.com>
2018-08-16 12:19:30 +0300

    Teht5.13 add test for like button

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -6,14 +6,27 @@ import SimpleBlog from './SimpleBlog'
 
 describe.only('<SimpleBlog />', () => {
 
-  it('renders title, author and likes', () => {
-    const testiblog = {
-      title: 'Test tittle for blog component',
-      author: 'Tiny Tester',
-      likes: 10
-    }
+  const testiblog = {
+    title: 'Test tittle for blog component',
+    author: 'Tiny Tester',
+    likes: 10
+  }
 
-    const blogTestComponent = shallow(<SimpleBlog blog={testiblog} />)
+  const mockHandler = jest.fn()
+  let blogTestComponent
+
+  beforeEach(() => {
+    blogTestComponent = shallow(<SimpleBlog blog={testiblog} onClick={mockHandler} />)
+  })
+
+  it('renders title, author and likes', () => {
+    // const testiblog = {
+    //   title: 'Test tittle for blog component',
+    //   author: 'Tiny Tester',
+    //   likes: 10
+    // }
+    //
+    // const blogTestComponent = shallow(<SimpleBlog blog={testiblog} />)
 
     const titleDiv = blogTestComponent.find('.titleAndAuthorDiv')
     const likes = blogTestComponent.find('.likesDiv')
@@ -21,4 +34,14 @@ describe.only('<SimpleBlog />', () => {
     expect(titleDiv.text()).toContain(`${testiblog.title} ${testiblog.author}`)
     expect(likes.text()).toContain(`blog has ${testiblog.likes} likes`)
   })
+
+  it('handles two clicks on the like button', () => {
+
+    const likeButton = blogTestComponent.find('button')
+    likeButton.simulate('click')
+    likeButton.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
+
 })
